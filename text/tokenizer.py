d988f2c30a02f15ccad9a3c954b8b4b258922ca2
cschaefer26 <c.schaefer.home@gmail.com>
2020-06-15 14:01:17 +0200

    No norm.

--- text/tokenizer.py
+++ text/tokenizer.py
@@ -10,7 +10,6 @@ class Tokenizer:
         self.symbols = list(symbols)
         self.symbol_id = {s: i for i, s in enumerate(symbols, 1)}
         self.id_symbol = {i: s for i, s in enumerate(symbols, 1)}
-        self.id_symbol[0] = '_'
 
     def encode(self, text: str) -> List[int]:
         text = self.cleaner(text)
